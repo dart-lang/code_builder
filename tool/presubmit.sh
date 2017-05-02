3884f6580bb71ca4aa1a9f927ca4e989107c5319
danrubel <danrubel@google.com>
2017-05-02 14:03:49 -0400

    only check dartfmt on stable

--- tool/presubmit.sh
+++ tool/presubmit.sh
@@ -3,14 +3,16 @@
 # Make sure dartfmt is run on everything
 # This assumes you have dart_style as a dev_dependency
 echo "Checking dartfmt..."
-NEEDS_DARTFMT="$(find lib test -name "*.dart" | xargs dartfmt -n)"
-if [[ ${NEEDS_DARTFMT} != "" ]]
-then
-  echo "FAILED"
-  echo "${NEEDS_DARTFMT}"
-  exit 1
+if [ "$TRAVIS_DART_VERSION" = "stable" ]; then
+  NEEDS_DARTFMT="$(find lib test -name "*.dart" | xargs dartfmt -n)"
+  if [[ ${NEEDS_DARTFMT} != "" ]]
+  then
+    echo "FAILED"
+    echo "${NEEDS_DARTFMT}"
+    exit 1
+  fi
+  echo "PASSED"
 fi
-echo "PASSED"
 
 # Make sure we pass the analyzer
 echo "Checking dartanalyzer..."
