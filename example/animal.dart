3a448322185ee8a6befd508416e79e71d8fc6162
Matan Lurey <matan@lurey.org>
2017-07-12 18:57:29 -0700

    Get ready to publish 2.0.0-alpha.

---
+++ example/animal.dart
@@ -0,0 +1,18 @@
+// Copyright (c) 2017, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+import 'package:code_builder/code_builder.dart';
+import 'package:dart_style/dart_style.dart';
+
+void main() {
+  final animal = new Class((b) => b
+    ..name = 'Animal'
+    ..extend = const Reference.localScope('Organism').toType()
+    ..methods.add(new Method.returnsVoid((b) => b
+      ..name = 'eat'
+      ..lambda = true
+      ..body = new Code((b) => b..code = 'print(\'Yum\')'))));
+  final emitter = const DartEmitter();
+  print(new DartFormatter().format('${animal.accept(emitter)}'));
+}
---
+++ example/scope.dart
@@ -0,0 +1,21 @@
+// Copyright (c) 2017, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+import 'package:code_builder/code_builder.dart';
+import 'package:dart_style/dart_style.dart';
+
+void main() {
+  final library = new File((b) => b.body.addAll([
+        new Method((b) => b
+          ..body = new Code((b) => b.code = '')
+          ..name = 'doThing'
+          ..returns = const Reference('Thing', 'package:a/a.dart').toType()),
+        new Method((b) => b
+          ..body = new Code((b) => b..code = '')
+          ..name = 'doOther'
+          ..returns = const Reference('Other', 'package:b/b.dart').toType()),
+      ]));
+  final emitter = new DartEmitter(new Allocator.simplePrefixing());
+  print(new DartFormatter().format('${library.accept(emitter)}'));
+}
