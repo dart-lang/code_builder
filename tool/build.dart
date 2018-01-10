ff032c1e261b6300bfb4207462923fba31aa3add
Kevin Moore <kevmoo@google.com>
2018-01-09 16:15:28 -0800

    Update to latest build_runner package

--- tool/build.dart
+++ tool/build.dart
@@ -1,3 +1,4 @@
+#!/usr/bin/env dart --checked
 // Copyright (c) 2017, the Dart project authors.  Please see the AUTHORS file
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
@@ -5,9 +6,17 @@
 import 'dart:async';
 
 import 'package:build_runner/build_runner.dart';
+import 'package:build_config/build_config.dart';
+import 'package:built_value_generator/built_value_generator.dart';
+import 'package:source_gen/source_gen.dart';
 
-import 'src/codegen.dart';
+Future main(List<String> args) => run(args, _actions);
 
-Future<Null> main() async {
-  await build([buildAction()], deleteFilesByDefault: true);
-}
+final _actions = <BuilderApplication>[
+  applyToRoot(
+    new PartBuilder([
+      const BuiltValueGenerator(),
+    ]),
+    generateFor: const InputSet(include: const ['lib/src/specs/**.dart']),
+  )
+];
