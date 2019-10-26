959222bc15e435d34146633c8f0aa6d76a264415
Dmytro Dashenkov <ymhik98@gmail.com>
2019-10-26 14:20:13 +0300

    Introduce `Expression.or` and `Expression.negate`

--- lib/src/specs/expression.dart
+++ lib/src/specs/expression.dart
@@ -28,6 +28,9 @@ part 'expression/literal.dart';
 abstract class Expression implements Spec {
   const Expression();
 
+  /// An empty expression.
+  static const _empty = CodeExpression(Code(''));
+
   @override
   R accept<R>(covariant ExpressionVisitor<R> visitor, [R context]);
 
@@ -42,6 +45,16 @@ abstract class Expression implements Spec {
   /// Returns the result of `this` `&&` [other].
   Expression and(Expression other) {
     return BinaryExpression._(expression, other, '&&');
+  }
+
+  /// Returns the result of `this` `||` [other].
+  Expression or(Expression other) {
+    return BinaryExpression._(expression, other, '||');
+  }
+
+  /// Returns the result of `!this`.
+  Expression negate() {
+    return BinaryExpression._(_empty, expression, '!');
   }
 
   /// Returns the result of `this` `as` [other].
