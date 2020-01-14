0d5e044cc8010912a5fedc9691608ddf2a9e3c2d
Nish Tahir <nishtahir@outlook.com>
2020-01-13 19:37:39 -0500

    Remove unused comment

--- lib/src/specs/extension.dart
+++ lib/src/specs/extension.dart
@@ -1,4 +1,4 @@
-// Copyright (c) 2017, the Dart project authors.  Please see the AUTHORS file
+// Copyright (c) 2020, the Dart project authors.  Please see the AUTHORS file
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
@@ -42,7 +42,6 @@ abstract class Extension extends Object
   BuiltList<Method> get methods;
   BuiltList<Field> get fields;
 
-  /// Name of the class.
   String get name;
 
   @override
