c15f4ca7ba43b4c14da67f3d370f9e6b0ed4ebe4
Kevin Moore <kevmoo@google.com>
2021-04-27 16:18:47 -0700

    Fix newly enforced lint

--- lib/src/specs/extension.dart
+++ lib/src/specs/extension.dart
@@ -2,8 +2,8 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-import 'package:built_value/built_value.dart';
 import 'package:built_collection/built_collection.dart';
+import 'package:built_value/built_value.dart';
 import 'package:meta/meta.dart';
 
 import '../../code_builder.dart';
